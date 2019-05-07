327f9274af9716eb955cbbe54eb5c26c9e564392
Sophia Song <ososanne@gmail.com>
2019-05-07 17:27:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
 
-end
+   if array.size == 1 
+    array.join
+  elsif array.size == 2 
+    array.join(" and ")
+  else
+    array[0...-1].join(", ") << ", and #{array[-1]}"
+  end
+end 	end 
